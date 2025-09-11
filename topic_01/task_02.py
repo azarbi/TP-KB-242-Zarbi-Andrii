63f27fa1dcce911a5c76eeee1b7d6bd3181fcd29
Andrey Zarbi <andriizarbi@gmail.com>
2025-09-11 20:11:21 +0300

    Update task_02.py

--- topic_01/task_02.py
+++ topic_01/task_02.py
@@ -1,11 +1,13 @@
 s = "  Hello world" 
 s1 = "python is the best"
+s2 = "PYTHON"
 def test():
     print("Strip: ", s.strip())
     print("Capitalize: ", s1.capitalize())
     print("Title: ", s1.title())
     print("Upper: ", s.upper())
-    print("Lower: ", s.lower())
+    print("Lower: ", s2.lower())
 print("Original 'Hello world': ", s)
 print("Original 'Python is the best': ", s1)
+print("Original 'Python': ", s2)
 test()
