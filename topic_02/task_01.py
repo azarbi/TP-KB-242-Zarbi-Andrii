e47588fd4ec1ea06ad8e0bd8c1d71be27618c95c
Andrey Zarbi <andriizarbi@gmail.com>
2025-09-19 14:35:16 +0300

    Added topic_02 folder and first task

---
+++ topic_02/task_01.py
@@ -0,0 +1,9 @@
+def discr(a, b, c):
+    return b * b - 4 * a * c
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+def findx(a, b, d):
+    return (-b + d**(0.5))/2*a
+    return (-b - d**(0.5))/2*a
+print(discr(a, b, c))
