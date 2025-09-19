04b4c78f17e922820d681b7bbc91a9376dc6bf7c
Andrey Zarbi <andriizarbi@gmail.com>
2025-09-19 14:38:09 +0300

    Update first task

--- topic_02/task_01.py
+++ topic_02/task_01.py
@@ -3,7 +3,17 @@ def discr(a, b, c):
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
-def findx(a, b, d):
-    return (-b + d**(0.5))/2*a
-    return (-b - d**(0.5))/2*a
+def findx(a, b, c):
+    d = discr(a, b, c)
+    if d > 0:
+     x1 = (-b + d**(0.5))/(2*a)
+     x2 = (-b - d**(0.5))/(2*a)
+     return f"Перший корінь: {x1}" \
+     f" Другий корінь: {x2}"
+    elif d == 0:
+     x = -b/(2*a)
+     return f"Рівняння має один корінь: {x}"
+    elif d < 0:
+     return "Рівняння не має розв'язку"
 print(discr(a, b, c))
+print(findx(a, b, c))
